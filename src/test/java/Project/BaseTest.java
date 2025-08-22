95b40be6785e96471ceacbdcd0e18662a4f5d678
Diwakar8686 <diw2akaraubr@gmail.com>
2025-08-22 12:24:22 +0530

    update file

--- src/test/java/Project/BaseTest.java
+++ src/test/java/Project/BaseTest.java
@@ -1,5 +1,28 @@
 package Project;
 
-public class BaseTest {
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+	import com.aventstack.extentreports.testng.listener.ExtentITestListenerClassAdapter;
+	import org.openqa.selenium.WebDriver;
+	import org.openqa.selenium.chrome.ChromeDriver;
+	import org.testng.annotations.*;
+	import io.github.bonigarcia.wdm.WebDriverManager;
 
-}
+	public class BaseTest {
+	    protected WebDriver driver;
+
+	    @BeforeClass
+	    public void setup() {
+	        WebDriverManager.chromedriver().setup();
+	        driver = new ChromeDriver();
+	        driver.manage().window().maximize();
+	        driver.get("https://www.tradingview.com/chart/");
+	    }
+
+	    @AfterClass(alwaysRun = true)
+	    public void teardown() {
+	        if (driver != null) driver.quit();
+	    }
+	}
+
+
---
+++ src/test/java/io/restassured/response/Response.java
@@ -0,0 +1,5 @@
+package io.restassured.response;
+
+public class Response {
+
+}
